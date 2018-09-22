156c986fcf35c436185553501ae3a045beadc129
Kasey Stowell <stowellk@eos12.cis.gvsu.edu>
2018-09-22 17:24:33 -0400

    multiple threads

--- threadedserver.c
+++ threadedserver.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_THREADS 10
+
 /*
  * Lab Assignment 4
  * Blocking Multi-threaded Server
@@ -17,12 +19,12 @@
 void* fileget(void* arg);
 
 int main() {
-	// testing with just one thread to start with
-	pthread_t thread1;
-	void *result1;
+	// multiple threads? not sure if best practice or not...
+	pthread_t threads[MAX_THREADS];
 	int status;
 	char input[256];
 	char* inputstr = input;
+	int counter = 0; // count spot in threads
 	
 	// infinite loop?
 	while(1) {
@@ -31,21 +33,25 @@ int main() {
 		scanf("%s", inputstr);
 		// add error check for empty string?
 		// create thread
-		if ((status = pthread_create(&thread1, NULL, fileget, (void*)inputstr)) != 0) {
+		if ((status = pthread_create(&threads[counter], NULL, fileget, (void*)inputstr)) != 0) {
 			fprintf(stderr, "thread create error %d: %s\n", status, strerror(status));
 			exit(1);
 		}
-		// join with thread
-		if ((status = pthread_join(thread1, &result1)) != 0) {
-			fprintf(stderr, "join error %d: %s\n", status,strerror(status));
-			exit(1);
-		}
+		// update counter
+		counter < MAX_THREADS ? (counter++) : (counter = 0);
 	}
 
 }
 
 void* fileget(void* arg) {
-	char* str = (char*) arg;
-	printf("\nThread got the string: %s\n", str);
+	char* str = malloc(strlen((char*) arg) + 1);
+	strcpy(str, (char*) arg);
+	srand(time(NULL));
+	// generate 80/20 probability for sleep time
+	float ran = (float)rand()/RAND_MAX;
+	ran <= 0.8 ? sleep(1) : sleep((7 + rand() % (10 + 1 - 7)));
+	// print diagnostic message.. probably can change so something more "official" later
+	printf("Thread got the string: %s and Random num: %f\n", str, ran);
+	free(str);
 	return NULL;
 }
