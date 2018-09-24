2ed6e1c40d5c90de6c8332bfe6aa2c1558de1248
Kehlsoo <lewiskeh@mail.gvsu.edu>
2018-09-24 15:58:40 -0400

    fixed memory leak

--- threadedserver.c
+++ threadedserver.c
@@ -38,20 +38,29 @@ int main() {
 
 	signal(SIGINT, sigUsrHandler); //creating SIGINT for termination
 	
-	// infinite loop?
+	// dispatcher reads user input, creates thread, detaches thread, and repeats
 	while(1) {
+		
 		// get user information
 		printf("\nEnter File Name: ");
 		scanf("%s", inputstr);
-		// add error check for empty string?
-		// create thread
-		if ((status = pthread_create(&threads[counter], NULL, fileget, (void*)inputstr)) != 0) {
-			fprintf(stderr, "thread create error %d: %s\n", status, strerror(status));
-			exit(1);
-		}
+
+			// create worker thread
+			if ((status = pthread_create(&threads[counter], NULL, fileget, (void*)inputstr)) != 0) {
+				fprintf(stderr, "thread create error %d: %s\n", status, strerror(status));
+				exit(1);
+			}
+
+		// detach the child to execute independently until termination
+			if ((status = pthread_detach (threads[counter])) != 0) {
+				fprintf(stderr, "thread detach error %d: %s\n", status, strerror(status));
+				exit(1);
+			}
+
 		// update counter
-		counter < MAX_THREADS ? (counter++) : (counter = 0);
+			counter < MAX_THREADS ? (counter++) : (counter = 0);		
 	}
+	return 0;
 
 }
 
@@ -59,13 +68,14 @@ void* fileget(void* arg) {
 	char* str = malloc(strlen((char*) arg) + 1);
 	strcpy(str, (char*) arg);
 	srand(time(NULL));
+
 	// generate 80/20 probability for sleep time
 	float ran = (float)rand()/RAND_MAX;
 	ran <= 0.8 ? sleep(1) : sleep((7 + rand() % (10 + 1 - 7)));
 
-	numFiles = numFiles + 1;
+	numFiles = numFiles + 1; //increment # of files done
 
-	// print diagnostic message.. probably can change so something more "official" later
+	//diagnostic message
 	printf("\nThread got the string: %s\n", str);
 	
 	free(str);
@@ -75,9 +85,9 @@ void* fileget(void* arg) {
 void sigUsrHandler(int signNum) { 
 	//terminates when control c recieved
 	if (signNum == SIGINT){
-		printf("Control-c registered.\n");
+		printf("\n\nControl-c registered.\n");
 		printf("Number of file requests recieved/serviced: %d\n", numFiles);
-		printf("Exiting...\n");
+		printf("Exiting...\n\n");
 		exit(0);
 	}
 }
